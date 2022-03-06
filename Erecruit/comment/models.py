ac495c458a6ad91e383f2c265162ccc0b4c28c47
qwe23432 <2660675754@qq.com>
2022-03-06 14:23:37 +0800

    在industry内写了主页，修改了comment/models.py

--- Erecruit/comment/models.py
+++ Erecruit/comment/models.py
@@ -2,12 +2,11 @@ from datetime import datetime
 
 from django.db import models
 
-
 # Create your models here.
 
 
-
 # 回复
+from company.models import Profession
 from user.models import User
 
 
@@ -15,3 +14,4 @@ class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="发表评论的用户")
     date = models.DateField(default=datetime.now, verbose_name="评论时间")
     detail = models.CharField(max_length=150, verbose_name="评论内容")
+    profession = models.ForeignKey(Profession, on_delete=models.CASCADE, verbose_name="评论的职位")
