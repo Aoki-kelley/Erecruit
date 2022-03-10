80310a08fe40fd664e8428676f807de0001de924
Aoki-kelley <darkkelley_w@163.com>
2022-03-10 22:58:38 +0800

    sixth
    用户部分完成

--- Erecruit/user/serializers.py
+++ Erecruit/user/serializers.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
 
 from .models import *
+from company.models import *
+from industry.models import *
+from comment.models import *
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -14,8 +17,58 @@ class UserSerializer(serializers.ModelSerializer):
         )
 
 
+class CompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Company
+        fields = (
+            'id',
+            'name',
+            'detail',
+            'applicant',
+            'img',
+        )
+
+
+class IndustrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Industry
+        fields = (
+            'id',
+            'name',
+            'work1',
+        )
+
+
+class EducationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Education
+        fields = (
+            'id',
+            'education',
+        )
+
+
+class ProfessionSerializer(serializers.ModelSerializer):
+    industry = IndustrySerializer(read_only=True)
+    company = CompanySerializer(read_only=True)
+    education = EducationSerializer(read_only=True)
+
+    class Meta:
+        model = Profession
+        fields = (
+            'id',
+            'industry',
+            'company',
+            'salary',
+            'name',
+            'education',
+            'application',
+        )
+
+
 class ResumeSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
+    education = EducationSerializer(read_only=True)
 
     class Meta:
         model = Resume
@@ -31,4 +84,32 @@ class ResumeSerializer(serializers.ModelSerializer):
             'wish_salary',
             'identity',
             'detail',
+        )
+
+
+class WishSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    profession = ProfessionSerializer(read_only=True)
+
+    class Meta:
+        model = Wish
+        fields = (
+            'id',
+            'user',
+            'profession',
+        )
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    profession = ProfessionSerializer(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = (
+            'id',
+            'user',
+            'date',
+            'detail',
+            'profession',
         )
--- Erecruit/user/views.py
+++ Erecruit/user/views.py
@@ -3,6 +3,7 @@ import simplejson
 
 from . import serializers
 from .models import *
+from comment.models import *
 from django.views.generic import View
 from django.http import JsonResponse
 
@@ -43,9 +44,13 @@ class Register(View):
         wish_salary_max = req.POST.get('wish_salary_max')
         wish_salary_min = req.POST.get('wish_salary_min')
         edu = Education.objects.filter(education__exact=education)
+        if sex not in ('man', 'woman'):
+            rep['code'] = 4040
+            rep['msg'] = u'意料外的错误导致修改失败，请稍后重试\n提示：性别填写有误'
+            return JsonResponse(rep)
         if wish_salary_min >= wish_salary_max:
             rep['code'] = 4040
-            rep['msg'] = u'期望薪资最小值大于等于最大值'
+            rep['msg'] = u'意料外的错误导致注册失败，请稍后重试\n提示：期望薪资最小值大于等于最大值'
             return JsonResponse(rep)
         if identity not in ('schooling', 'graduate', 'working'):
             rep['code'] = 4040
@@ -108,10 +113,11 @@ class Login(View):
                 rep['msg'] = u'意料外的错误导致登录失败，请稍后重试'
                 return JsonResponse(rep)
             target.delete()
-            req.session['is_login'] = True
+            req.session['uid'] = user.id
             req.session.set_expiry(86400)
             rep['code'] = 2000
             rep['msg'] = u'登录成功'
+            rep['data'] = dict()
             rep['data']['user'] = serializer.data
         else:
             rep['code'] = 4040
@@ -126,12 +132,12 @@ class HomePage(View):
         """
         根据前端返回的用户id，查询用户，并判断当前登录的是否为本人
         """
-        uid = req.GET.get('id')
         rep = {
             'code': 0,
             'msg': ''
         }
-        is_login = req.session.get('is_login', None)
+        uid = req.GET.get('id')
+        is_login = req.session.get('uid', None)
         try:
             user = User.objects.filter(id=uid)[0]
             serializer = serializers.UserSerializer(user)
@@ -144,14 +150,14 @@ class HomePage(View):
         rep['msg'] = u'获取成功'
         rep['data'] = dict()
         rep['data']['user'] = serializer.data
-        if is_login is True:
+        if is_login == uid:
             rep['data']['is_oneself'] = True
         else:
             rep['data']['is_oneself'] = False
         return JsonResponse(rep)
 
 
-# 用户名更改(PUT)
+# 用户名更改(PUT)  已完成
 class ResetName(View):
     # noinspection PyMethodMayBeStatic
     def put(self, req):
@@ -165,6 +171,11 @@ class ResetName(View):
         get_data = simplejson.loads(req.body)
         uid = get_data['id']
         new_name = get_data['new_name']
+        is_login = req.session.get('uid', None)
+        if is_login != uid:
+            rep['code'] = 4040
+            rep['msg'] = u'请求用户与登录用户不一致'
+            return JsonResponse(rep)
         try:
             user = User.objects.filter(id=uid)[0]
         except Exception as e:
@@ -185,15 +196,23 @@ class ResetName(View):
         return JsonResponse(rep)
 
 
-# 个人简历(GET,PUT) GET测完
+# 个人简历(GET,PUT) GET,PUT已完成
 class MineResume(View):
     # noinspection PyMethodMayBeStatic
     def get(self, req):
+        """
+        根据前端返回的用户id查询简历并返回
+        """
         uid = req.GET.get('id')
         rep = {
             'code': 0,
             'msg': ''
         }
+        is_login = req.session.get('uid', None)
+        if is_login != uid:
+            rep['code'] = 4040
+            rep['msg'] = u'请求用户与登录用户不一致'
+            return JsonResponse(rep)
         try:
             user = User.objects.filter(id=uid)[0]
             resume = Resume.objects.filter(user_id=user.id)[0]
@@ -209,18 +228,220 @@ class MineResume(View):
         rep['data']['resume'] = serializer.data
         return JsonResponse(rep)
 
+    # noinspection PyMethodMayBeStatic
     def put(self, req):
-        pass
+        """
+        根据前端返回的简历id和简历数据修改简历
+        """
+        rep = {
+            'code': 0,
+            'msg': ''
+        }
+        rid = req.GET.get('id')
+        get_data = simplejson.loads(req.body)['data']
+        name = get_data['name']
+        sex = get_data['sex']
+        education = get_data['education']
+        identity = get_data['identity']
+        major = get_data['major']
+        wish_job = get_data['wish_job']
+        wish_city = get_data['wish_job']
+        wish_salary_max = get_data['wish_salary_max']
+        wish_salary_min = get_data['wish_salary_min']
+        edu = Education.objects.filter(education__exact=education)
+        if wish_salary_min >= wish_salary_max:
+            rep['code'] = 4040
+            rep['msg'] = u'意料外的错误导致修改失败，请稍后重试\n提示：期望薪资最小值大于等于最大值'
+            return JsonResponse(rep)
+        if not edu:
+            rep['code'] = 4040
+            rep['msg'] = u'意料外的错误导致修改失败，请稍后重试\n提示：学历填写有误'
+            return JsonResponse(rep)
+        if identity not in ('schooling', 'graduate', 'working'):
+            rep['code'] = 4040
+            rep['msg'] = u'意料外的错误导致修改失败，请稍后重试\n提示：身份填写有误'
+            return JsonResponse(rep)
+        if sex not in ('man', 'woman'):
+            rep['code'] = 4040
+            rep['msg'] = u'意料外的错误导致修改失败，请稍后重试\n提示：性别填写有误'
+            return JsonResponse(rep)
+        try:
+            resume = Resume.objects.filter(id=rid)[0]
+            resume.name = name
+            resume.sex = sex
+            resume.education = edu[0]
+            resume.identity = identity
+            resume.major = major
+            resume.wish_job = wish_job
+            resume.wish_city = wish_city
+            resume.wish_salary = str(wish_salary_max) + 'k-' + str(wish_salary_min) + 'k'
+            resume.save()
+        except Exception as e:
+            print('出现错误', repr(e))
+            rep['code'] = 4040
+            rep['msg'] = u'意料外的错误导致修改失败，请稍后重试'
+            return JsonResponse(rep)
+        rep['code'] = 2000
+        rep['msg'] = u'修改成功'
+        return JsonResponse(rep)
 
 
-# 个人收藏职位(GET,DEL)
+# 个人收藏职位(GET,DEL)  GET,DEL已完成
 class WishProfession(View):
-    pass
+    # noinspection PyMethodMayBeStatic
+    def get(self, req):
+        """
+        根据前端返回的用户id，查询该用户收藏的职位并返回
+        """
+        rep = {
+            'code': 0,
+            'msg': ''
+        }
+        uid = req.GET.get('id')
+        is_login = req.session.get('uid', None)
+        if is_login != uid:
+            rep['code'] = 4040
+            rep['msg'] = u'请求用户与登录用户不一致'
+            return JsonResponse(rep)
+        try:
+            user = User.objects.filter(id=uid)[0]
+            wish = Wish.objects.filter(user_id=user.id)
+        except Exception as e:
+            print('出现错误', repr(e))
+            rep['code'] = 4040
+            rep['msg'] = u'查询的用户或收藏的职位不存在'
+            return JsonResponse(rep)
+        rep['code'] = 2000
+        rep['msg'] = u'查询成功'
+        rep['data'] = dict()
+        if len(wish) == 1:
+            serializer = serializers.WishSerializer(wish[0])
+            rep['data']['wish'] = serializer.data
+        elif len(wish) >= 2:
+            serializer = serializers.WishSerializer(wish, many=True)
+            rep['data']['wish'] = serializer.data
+        return JsonResponse(rep)
+
+    # noinspection PyMethodMayBeStatic
+    def delete(self, req):
+        """
+        根据前端返回的用户id和收藏id,判断是否匹配
+        若匹配，则尝试删除；否则返回错误信息
+        """
+        get_data = simplejson.loads(req.body)
+        rep = {
+            'code': 0,
+            'msg': '',
+        }
+        uid = get_data['user_id']
+        wid = get_data['wish_id']
+        is_login = req.session.get('uid', None)
+        if is_login != uid:
+            rep['code'] = 4040
+            rep['msg'] = u'请求用户与登录用户不一致'
+            return JsonResponse(rep)
+        try:
+            user = User.objects.filter(id=uid)[0]
+            wish = Wish.objects.filter(id=wid)[0]
+        except Exception as e:
+            print('出现错误', repr(e))
+            rep['code'] = 4040
+            rep['msg'] = u'查询的用户或收藏的职位不存在'
+            return JsonResponse(rep)
+        if wish.user == user:
+            try:
+                wish.delete()
+            except Exception as e:
+                print('出现错误', repr(e))
+                rep['code'] = 4040
+                rep['msg'] = u'意料外的错误导致操作失败'
+                return JsonResponse(rep)
+            rep['code'] = 2000
+            rep['msg'] = u'删除成功'
+            return JsonResponse(rep)
+        else:
+            rep['code'] = 4040
+            rep['msg'] = u'待删除的收藏和所属用户不对应'
+            return JsonResponse(rep)
 
 
-# 个人评论(GET,DEL)
+# 个人评论(GET,DEL) GET,DEL已完成
 class MineComment(View):
-    pass
+    # noinspection PyMethodMayBeStatic
+    def get(self, req):
+        """
+        根据前端返回的用户id查询评论并返回
+        """
+        rep = {
+            'code': 0,
+            'msg': '',
+        }
+        uid = req.GET.get('id')
+        is_login = req.session.get('uid', None)
+        if is_login != uid:
+            rep['code'] = 4040
+            rep['msg'] = u'请求用户与登录用户不一致'
+            return JsonResponse(rep)
+        try:
+            user = User.objects.filter(id=uid)[0]
+            comments = Comment.objects.filter(user=user)
+        except Exception as e:
+            print('出现错误', repr(e))
+            rep['code'] = 4040
+            rep['msg'] = u'查询的用户不存在'
+            return JsonResponse(rep)
+        rep['code'] = 2000
+        rep['msg'] = u'查询成功'
+        rep['data'] = dict()
+        if len(comments) == 1:
+            serializer = serializers.CommentSerializer(comments[0])
+            rep['data']['comment'] = serializer.data
+        elif len(comments) >= 2:
+            serializer = serializers.CommentSerializer(comments, many=True)
+            rep['data']['comment'] = serializer.data
+        return JsonResponse(rep)
+
+    # noinspection PyMethodMayBeStatic
+    def delete(self, req):
+        """
+        根据前端返回的用户id和评论id,判断是否匹配
+        若匹配，则尝试删除；否则返回错误信息
+        """
+        get_data = simplejson.loads(req.body)
+        rep = {
+            'code': 0,
+            'msg': '',
+        }
+        uid = get_data['user_id']
+        cid = get_data['comment_id']
+        is_login = req.session.get('uid', None)
+        if is_login != uid:
+            rep['code'] = 4040
+            rep['msg'] = u'请求用户与登录用户不一致'
+            return JsonResponse(rep)
+        try:
+            user = User.objects.filter(id=uid)[0]
+            comment = Comment.objects.filter(id=cid)[0]
+        except Exception as e:
+            print('出现错误', repr(e))
+            rep['code'] = 4040
+            rep['msg'] = u'查询的用户或评论不存在'
+            return JsonResponse(rep)
+        if comment.user == user:
+            try:
+                comment.delete()
+            except Exception as e:
+                print('出现错误', repr(e))
+                rep['code'] = 4040
+                rep['msg'] = u'意料外的错误导致操作失败'
+                return JsonResponse(rep)
+            rep['msg'] = u'删除成功'
+            rep['code'] = 2000
+            return JsonResponse(rep)
+        else:
+            rep['code'] = 4040
+            rep['msg'] = u'待删除的评论和所属用户不对应'
+            return JsonResponse(rep)
 
 
 # 发送邮件验证码(GET) 已完成
